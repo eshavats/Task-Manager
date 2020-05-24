94bb576d328ef050bed5299923851a1eca479eba
Esha Vats <56391290+eshavats@users.noreply.github.com>
2020-05-25 01:36:21 +0530

    Make get and post requests

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,85 +1,8 @@
 const mongoose = require("mongoose");
-const validator = require("validator");
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", { 
-    useNewUrlParser: true ,
-    useCreateIndex: true 
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false 
 });
 
-const User = mongoose.model("User", {
-   name: {
-    type: String,
-    required: true,
-    trim: true
-   },
-   email : {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(value) {
-        if(!validator.isEmail(value))
-        {
-            throw new Error("Email is invalid!");
-        }
-    }
-   },
-   age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-        if(value < 0)
-        {
-            throw new Error("Age must be a positive number!");
-        }
-    }
-   },
-   password: {
-       type: String,
-       required: true,
-       trim: true,
-       minlength: 7,
-       validate(value) {
-           if(value.toLowerCase().includes("password"))
-           {
-               throw new Error('Password cannot contain "password"');
-           }
-       }
-   } 
-});
-
-// const me = new User({
-//     name: "Nikhil",
-//     email: "nikhil@gmail.com",
-//     age: 20,
-//     password: "hrufheirj123"
-// });
-
-// me.save().then(() => {
-//     console.log(me);    
-// }).catch((error) => {
-//     console.log(error);    
-// });
-
-const Task = mongoose.model("Task", {
-    description: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-});
-
-// const buy = new Task({
-//     description: "Complete HW"
-
-// });
-
-// buy.save().then(() => {
-//     console.log(buy);    
-// }).catch((error) => {
-//     console.log(error);    
-// });
