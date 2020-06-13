ac67df29d97af98793b2a55e7cd4baf84e600404
Esha Vats <56391290+eshavats@users.noreply.github.com>
2020-06-14 02:14:36 +0530

    generate authentication tokens

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -42,8 +43,24 @@ const userSchema = new mongoose.Schema({
                 throw new Error('Password cannot contain "password"');
             }
         }
-    } 
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }] 
  });
+
+ userSchema.methods.generateAuthToken = async function() {
+     const user = this;
+     const token = await jwt.sign({ _id: user._id.toString() }, "thisismyapp");
+
+     user.tokens = user.tokens.concat({ token });
+     await user.save();
+
+     return token;
+ }
 
  //To find the user when he logs in
 userSchema.statics.findByCredentials = async (email, password) => {
